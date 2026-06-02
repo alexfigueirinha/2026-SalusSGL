424650de5fabf105b7ed062429c05b095c1ce003
analuuvk-creator <analuuvk@gmail.com>
2026-06-02 15:33:47 -0300

    internacao commit

--- resources/views/livewire/internacao/internacaos-index.blade.php
+++ resources/views/livewire/internacao/internacaos-index.blade.php
@@ -69,90 +69,66 @@
                 </ul>
             </div>
 
-    <div class="container">
-        @if (session()->has('error'))
-            <div class="alert alert-danger">
-                {{ session('error') }}
-            </div>
-        @endif
+            <div class="container">
+                <div class="d-flex justify-content-between align-items-center mb-4">
+                    <h2 class="mt-4">Internações</h2>
+                    <div class="d-flex gap-2">
+                        <a class="btn btn-primary mt-4" href="{{ route('internacao.create') }}">Registrar Internação</a>
+                    </div>
+                </div>
 
-        @if (session()->has('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
-        @endif
+                @if (session()->has('error'))
+                    <div class="alert alert-danger">
+                        {{ session('error') }}
+                    </div>
+                @endif
 
-        <div class="mb-3">
-            <input type="text" wire:model.live='search' placeholder="Pesquisar..." class="form-control">
-        </div>
+                @if (session()->has('success'))
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
+                @endif
 
-        <table class="table table-striped table-bordered">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Ala</th>
-                    <th>Quarto</th>
-                    <th>Leito</th>
-                    <th>Paciente</th>
-                    <th>Data e Hora de Entrada</th>
-                    <th>Ações</th>
-                </tr>
-            </thead>
+                <div class="mb-3">
+                    <input type="text" wire:model.live='search' placeholder="Pesquisar..." class="form-control">
+                </div>
 
-            <tbody>
-                @foreach ($internacaos as $internacao)
-                    <tr>
-<<<<<<< HEAD
-                        <td>{{ $internacao->id }}</td>
-                        <td>{{ $internacao->alas_id }}</td>
-                        <td>{{ $internacao->quartos_id }}</td>
-                        <td> {{ $internacao->leitos_id }}
-                        <td>{{ $internacao->pacientes_id }}</td>
-                        <td>{{ \Carbon\Carbon::parse($internacao->data_hora_entrada)->format('d/m/Y H:i:s') }}</td>
-                        <td>
-                            <a href="{{ route('internacao.edit', ['id' => $internacao->id]) }}"
-                                class="btn btn-primary btn-sm">Editar</a>
-
-                            <button class="btn btn-danger btn-sm" wire:click="excluir({{ $internacao->id }})"
-                                wire:confirm="Deseja excluir o quarto?">Excluir</button>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-=======
-                        <th>#</th>
-                        <th>Paciente</th>
-                        <th>Ala</th>
-                        <th>Quarto</th>
-                        <th>Leito</th>
-                        <th>Data e Hora de Entrada</th>
-                        <th>Ações</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    @foreach ($internacaos as $internacao)
+                <table class="table table-hover">
+                    <thead>
                         <tr>
-                            <td>{{ $internacao->id }}</td>
-                            <td>{{ $internacao->pacientes->nome }}</td>
-                            <td>{{ $internacao->alas_id }}</td>
-                            <td>{{ $internacao->quartos_id }}</td>
-                            <td>{{ $internacao->leitos_id }}</td>
-                            <td>{{ \Carbon\Carbon::parse($internacao->data_hora_entrada)->format('d/m/Y H:i:s') }}</td>
-                            <td>
-                                <a href="{{ route('internacao.edit', ['id' => $internacao->id]) }}"
-                                    class="btn btn-primary btn-sm">Editar</a>
-
-                                <button class="btn btn-danger btn-sm"
-                                    wire:click="excluir({{ $internacao->id }})"
-                                    wire:confirm="Deseja excluir o quarto?">Excluir</button>
-                            </td>
+                            <th>#</th>
+                            <th>Paciente</th>
+                            <th>Ala</th>
+                            <th>Quarto</th>
+                            <th>Leito</th>
+                            <th>Data e Hora de Entrada</th>
+                            <th>Ações</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+
+                    <tbody>
+                        @foreach ($internacaos as $internacao)
+                            <tr>
+                                <td>{{ $internacao->id }}</td>
+                                <td>{{ $internacao->pacientes->nome }}</td>
+                                <td><b>ID:</b> {{ $internacao->alas_id }} - <b>ALA:</b> {{ $internacao->alas->nome }}
+                                </td>
+                                <td><b>ID:</b> {{ $internacao->quartos_id }} - <b>QUARTO:</b>
+                                    {{ $internacao->quartos->quarto }}</td>
+                                <td><b>ID:</b> {{ $internacao->leitos_id }} - <b>LEITO:</b>
+                                    {{ $internacao->leito->leito }}</td>
+                                <td>{{ \Carbon\Carbon::parse($internacao->data_hora_entrada)->format('d/m/Y H:i:s') }}
+                                </td>
+                                <td>
+                                    <a href="{{ route('internacao.edit', ['id' => $internacao->id]) }}"
+                                        class="btn btn-primary btn-sm">Editar</a>
+                                    <button class="btn btn-danger btn-sm" wire:click="excluir({{ $internacao->id }})"
+                                        wire:confirm="Deseja excluir o quarto?">Excluir</button>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
->>>>>>> e18a56413ba2e257a9d1ebb7dce529a2213c5f25
     </div>
-</div>
